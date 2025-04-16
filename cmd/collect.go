45a10e13d48313633365b0b1c98483f9c2329da6
Ilia Medvedev <ilia.medvedev@codefresh.io>
2025-04-16 19:36:33 +0300

    works but don't know why

--- cmd/collect.go
+++ cmd/collect.go
@@ -1,62 +1,88 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 
 	s3cli "github.com/ilia-medvedev-codefresh/aws-s3-otel-metrics/pkg/s3_client"
+	telemetry "github.com/ilia-medvedev-codefresh/aws-s3-otel-metrics/pkg/telemetry"
 	"github.com/spf13/cobra"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/metric"
 )
 
 // collectCmd represents the collect command
 var collectCmd = &cobra.Command{
 	Use:   "collect",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Run collection once",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("collect called")
-		region, err := cmd.Flags().GetString("region")
+
+		region, _ := cmd.Flags().GetString("region")
+
+		if region == "" {
+			if reg, exists := os.LookupEnv("AWS_REGION"); !exists {
+				fmt.Println("Error: AWS region not specified. Use --region flag or set AWS_REGION environment variable.")
+				return
+			} else {
+				region = reg
+			}
+		}
+
+		err, s3client := s3cli.NewS3Client(region)
 
 		if err != nil {
-			fmt.Println("Error getting region flag:", err)
+			fmt.Println("Error creating S3 client:", err)
 			return
 		}
 
 
-		sess, err := session.NewSession(&aws.Config{
-			Region: aws.String(region), // Replace with your region
-		})
+		buckets, _ := cmd.Flags().GetStringArray("bucket")
 
-		if err != nil {
-			fmt.Println("Error creating session:", err)
+		if len(buckets) == 0 {
+			fmt.Println("Error: No S3 buckets specified")
 			return
 		}
 
-		svc := s3.New(sess)
+		keyAggregationDepth, _ := cmd.Flags().GetInt("key-aggregation-depth")
 
-		buckets, err := cmd.Flags().GetStringArray("bucket")
+		exp, err := stdoutmetric.New()
 
 		if err != nil {
-			fmt.Println("Error getting bucket flag:", err)
-			return
+			log.Fatal(err)
 		}
+
+		meter, err := telemetry.NewMeter(context.TODO(), exp)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		gauage, _ := meter.Meter.Float64Gauge("s3.aggregated.object.size.bytes", metric.WithDescription("Aggregated object size in bytes"), metric.WithUnit("Bytes"))
 
 		for _, bucket := range buckets {
-			err := s3cli.ListObjects(svc, bucket)
+			err, objects := s3client.ListObjectSizeBytes(bucket, keyAggregationDepth)
+
 			if err != nil {
 				fmt.Println("Error listing objects:", err)
 				return
+			}
+
+			for k,v := range objects {
+				gauage.Record(meter.Context, float64(v), metric.WithAttributes(
+					attribute.String("bucket", bucket),
+					attribute.String("aggregate.key", k),
+					))
+				//fmt.Printf("Bucket: %s, Object: %s, Size: %d bytes\n", bucket, k, v)
+			}
+
+			err = meter.Collect()
+
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
 	},
@@ -66,14 +92,6 @@ func init() {
 	rootCmd.AddCommand(collectCmd)
 	collectCmd.Flags().String("region", "", "AWS region")
 	collectCmd.Flags().StringArray("bucket", []string{}, "List of S3 buckets to collect metrics from")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// collectCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// collectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	collectCmd.Flags().Int("key-aggregation-depth", 0, "Key depth for object size aggregation metric")
+	collectCmd.MarkFlagRequired("bucket")
 }
--- pkg/s3_client/s3_client.go
+++ pkg/s3_client/s3_client.go
@@ -1,26 +1,73 @@
 package s3_client
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func ListObjects(svc *s3.S3, bucket string) error {
+type S3Client struct {
+	Client *s3.S3
+}
+
+func NewS3Client(region string) (error, *S3Client) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		return err, nil
+	}
+
+	client := s3.New(sess)
+
+	return nil, &S3Client{
+		Client: client,
+	}
+}
+
+func(c *S3Client) ListObjectSizeBytes(bucket string, depth int) (error, map[string]int64) {
+
+	sizeMap := make(map[string]int64)
+
 	// Set the parameters for listing objects in the S3 bucket
 	params := &s3.ListObjectsV2Input{
 		Bucket: aws.String(bucket),
 	}
 
 	// Paginate through all objects in the bucket
-	err := svc.ListObjectsV2Pages(params, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+	err := c.Client.ListObjectsV2Pages(params, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+
 		for _, obj := range page.Contents {
-			// Print object key and size
-			fmt.Printf("Object Key: %s, Size: %d bytes\n", *obj.Key, *obj.Size)
+
+				key := strings.Join(splitKeyByDepth(*aws.String(*obj.Key), depth), "/")
+
+				if _,hasKey := sizeMap[key]; hasKey {
+					sizeMap[key] += *obj.Size
+				} else {
+					sizeMap[key] = *obj.Size
+				}
 		}
+
 		return true
 	})
 
-	return err
+	return err, sizeMap
+}
+
+func splitKeyByDepth(key string, depth int) []string {
+	splitObject := strings.Split(key, "/")
+
+	if depth > 0 && depth < len(splitObject) {
+		keyParts := make([]string, depth)
+
+		for i := 0; i < depth && i < len(splitObject); i++ {
+			keyParts[i] = splitObject[i]
+		}
+
+		return keyParts
+	} else {
+		return splitObject
+	}
 }
